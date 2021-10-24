fee99177c13a251041da3d9607ad3da0962e2024
Daris <daris.hodzic.dh@gmail.com>
2021-10-24 13:07:04 +0200

    started

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import { ChatEngine } from "react-chat-engine";
 import "./App.css";
+import ChatFeed from "./components/ChatFeed";
 
 const App = () => {
-	return (
-		<ChatEngine
-			height="100vh"
-			projectID=""
-			userName=""
-			userSecret=""
-		/>
-	);
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="
+  3579c712-28ac-485c-a038-aa41e288d049"
+      userName="darishodzic"
+      userSecret="1234"
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+    />
+  );
 };
 
 export default App;
---
+++ src/components/ChatFeed.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import MessageForm from "./MessageForm";
+import MyMessage from "./MyMessage";
+
+const ChatFeed = (props) => {
+  console.log(props);
+  return <div>Chat Feed</div>;
+};
+
+export default ChatFeed;
