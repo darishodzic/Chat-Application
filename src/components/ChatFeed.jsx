2753d374f4fedafcd01258b2cd9bf45bb0c29ad8
Daris <daris.hodzic.dh@gmail.com>
2021-10-24 13:37:11 +0200

    chatFeed

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -3,7 +3,24 @@ import MessageForm from "./MessageForm";
 import MyMessage from "./MyMessage";
 
 const ChatFeed = (props) => {
-  console.log(props);
+  const { chats, activeChat, userName, messages } = props;
+
+  const chat = chats && chats[activeChat];
+
+  const renderMessages = () => {
+    const keys = Object.keys(messages);
+
+    return keys.map((key, index) => {
+      const message = messages[key];
+      const lastMessageKey = index === 0 ? null : keys[index - 1];
+      const isMyMessage = userName === message.sender.username;
+
+      return <div key={`msg_${index}`} style={{ width: "100%" }}></div>;
+    });
+  };
+
+  renderMessages();
+
   return <div>Chat Feed</div>;
 };
 
