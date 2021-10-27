f7a5471bab798910f124407503d2774cdee49ab4
Daris <daris.hodzic.dh@gmail.com>
2021-10-27 17:10:14 +0200

    login user

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import { ChatEngine } from "react-chat-engine";
 import "./App.css";
 import ChatFeed from "./components/ChatFeed";
+import LoginForm from "./components/LoginForm";
 
 const App = () => {
+  if (!localStorage.getItem("username")) return <LoginForm />;
+
   return (
     <ChatEngine
       height="100vh"
       projectID="
   3579c712-28ac-485c-a038-aa41e288d049"
-      userName="darishodzic"
-      userSecret="1234"
+      userName={localStorage.getItem("username")}
+      userSecret={localStorage.getItem("password")}
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
     />
   );
